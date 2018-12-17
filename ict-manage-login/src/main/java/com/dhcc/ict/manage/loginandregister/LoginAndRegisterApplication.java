ffe77f907bce8a1da8db99b2b895d4c40b819925
CuiYang16 <cuiyangmail@163.com>
2018-12-17 09:26:49 +0800

    c

--- ict-manage-login/src/main/java/com/dhcc/ict/manage/loginandregister/LoginAndRegisterApplication.java
+++ ict-manage-login/src/main/java/com/dhcc/ict/manage/loginandregister/LoginAndRegisterApplication.java
@@ -1,4 +1,4 @@
-package com.dhcc.ict.manage.login;
+package com.dhcc.ict.manage.loginandregister;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class LoginApplication {
+public class LoginAndRegisterApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(LoginApplication.class, args);
+		SpringApplication.run(LoginAndRegisterApplication.class, args);
 	}
 }
