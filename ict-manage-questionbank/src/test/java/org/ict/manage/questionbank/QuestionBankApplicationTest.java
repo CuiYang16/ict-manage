a431a2ccb9a40e109b26231d7eb3b7ee2c3bd6ca
CuiYang16 <cuiyangmail@163.com>
2018-12-18 11:26:27 +0800

    c

---
+++ ict-manage-questionbank/src/test/java/org/ict/manage/questionbank/QuestionBankApplicationTest.java
@@ -0,0 +1,20 @@
+package org.ict.manage.questionbank;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class QuestionBankApplicationTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
